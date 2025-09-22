f1fef3e5a08b9ffcbceda447fa7022ec278d5ffb
Michael Aerni <hello@michaelaerni.ch>
2025-09-22 12:46:01 +0200

    Add more checks

--- src/CloudflaredServiceProvider.php
+++ src/CloudflaredServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Aerni\Cloudflared;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class CloudflaredServiceProvider extends ServiceProvider
@@ -12,10 +13,24 @@ class CloudflaredServiceProvider extends ServiceProvider
             return;
         }
 
-        if (request()->root() !== env('CLOUDFLARED_APP_URL')) {
+        if (! $this->isRunningViteServer()) {
+            return;
+        }
+
+        if (! $this->isCloudflaredRequest()) {
             return;
         }
 
         config()->set('app.url', env('CLOUDFLARED_APP_URL'));
+    }
+
+    protected function isRunningViteServer(): bool
+    {
+        return File::exists(public_path('hot'));
+    }
+
+    protected function isCloudflaredRequest(): bool
+    {
+        return request()->host() === parse_url(env('CLOUDFLARED_APP_URL'), PHP_URL_HOST);
     }
 }
