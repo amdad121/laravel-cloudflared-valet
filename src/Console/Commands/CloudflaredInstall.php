9ffd3c59da350c56ca76bd3a6424660379f9abf7
Michael Aerni <hello@michaelaerni.ch>
2025-09-24 12:56:53 +0200

    Ask for DNS overwrite permission at the start

--- src/Console/Commands/CloudflaredInstall.php
+++ src/Console/Commands/CloudflaredInstall.php
@@ -2,24 +2,21 @@
 
 namespace Aerni\Cloudflared\Console\Commands;
 
-use Illuminate\Process\Pool;
 use Illuminate\Console\Command;
-use function Laravel\Prompts\info;
-use function Laravel\Prompts\text;
-use function Laravel\Prompts\form;
-use function Laravel\Prompts\spin;
 use Illuminate\Support\Facades\File;
-use function Laravel\Prompts\confirm;
 use Illuminate\Support\Facades\Process;
-use Aerni\Cloudflared\Console\Commands\CloudflaredUninstall;
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\info;
+use function Laravel\Prompts\spin;
+use function Laravel\Prompts\text;
+use RuntimeException;
 
 class CloudflaredInstall extends Command
 {
-    protected $signature = 'cloudflared:install {--force : Overwrite any existing CNAME records.}';
+    protected $signature = 'cloudflared:install';
 
     protected $description = 'Create a Cloudflare Tunnel for this project.';
 
-    protected string $herdSite;
     protected string $hostname;
     protected string $subdomain;
     protected string $tunnelId;
@@ -38,18 +35,29 @@ class CloudflaredInstall extends Command
         //     return;
         // }
 
-        $this->herdSite = basename(base_path());
+        $herdSite = basename(base_path());
 
         $this->hostname = text(
             label: 'What hostname do you want to associate with this tunnel?',
-            placeholder: "{$this->herdSite}.domain.com",
+            placeholder: "{$herdSite}.domain.com",
             hint: 'It is recommended to match the subdomain to your Laravel Herd site.',
             validate: fn (string $value) => match (true) {
                 empty($value) => 'The hostname field is required.',
-                count(array_filter(explode('.', $value))) < 3 => "The hostname must include a subdomain (e.g., {$this->herdSite}.domain.com).",
+                count(array_filter(explode('.', $value))) < 3 => "The hostname must include a subdomain (e.g., {$herdSite}.domain.com).",
                 default => null,
             },
         );
+
+        $continue = confirm(
+            label: "Do you want to overwrite potentially existing DNS records for \"{$this->hostname}\" and \"vite-{$this->hostname}\"?",
+            yes: 'Yes, continue',
+            no: 'No, abort',
+            hint: 'The terms must be accepted to continue.'
+        );
+
+        if (! $continue) {
+            return;
+        }
 
         $this->createCloudflaredTunnel();
         $this->createCloudflaredFile();
@@ -66,21 +74,21 @@ class CloudflaredInstall extends Command
         );
 
         if ($result->failed()) {
-            throw new \RuntimeException($result->errorOutput());
+            throw new RuntimeException($result->errorOutput());
         }
 
         if (! preg_match('/([^\s]+\.json)(?=$|\s|\.)/', $result->output(), $credentialsPathMatch)) {
-            throw new \RuntimeException("Unable to extract the credentials file path.");
+            throw new RuntimeException('Unable to extract the credentials file path.');
         }
 
         if (! preg_match('/Created tunnel .+ with id ([a-f0-9\-]+)/', $result->output(), $tunnelMatch)) {
-            throw new \RuntimeException("Unable to extract the tunnel ID.");
+            throw new RuntimeException('Unable to extract the tunnel ID.');
         }
 
         $this->tunnelId = $tunnelMatch[1];
         $this->credentialsFilePath = $credentialsPathMatch[1];
 
-        info("<info>[✔]</info> Created tunnel.");
+        info('<info>[✔]</info> Created tunnel.');
     }
 
     protected function createCloudflaredFile(): void
@@ -94,73 +102,29 @@ YAML;
         File::put('.cloudflared.yaml', $config);
     }
 
-    protected function createAppDnsRecord(bool $force = false): void
+    protected function createAppDnsRecord(): void
     {
-        $command = $this->option('force') ?? $force
-            ? "cloudflared tunnel route dns --overwrite-dns {$this->hostname} {$this->hostname}"
-            : "cloudflared tunnel route dns {$this->hostname} {$this->hostname}";
-
         $result = spin(
-            callback: fn () => Process::run($command),
+            callback: fn () => Process::run("cloudflared tunnel route dns --overwrite-dns {$this->hostname} {$this->hostname}"),
             message: "Creating DNS record: {$this->hostname} …"
         );
 
-        if ($result->seeInErrorOutput('Failed to add route: code: 1003')) {
-            $confirmed = confirm(
-                label: "A DNS record for {$this->hostname} already exists. Overwrite?",
-                yes: 'Yes, overwrite the record.',
-                no: 'No, abort and delete the tunnel.',
-            );
-
-            // If this is true, we need to stop command execution.
-            // We should not continue to the ViteDnsRecord method or the Herd link method.
-            if (!$confirmed) {
-                $this->callSilently(CloudflaredUninstall::class);
-                $this->fail('The tunnel was deleted.');
-            }
-
-            $this->createAppDnsRecord(true);
-
-            return;
-        }
-
         if ($result->failed()) {
-            throw new \RuntimeException($result->errorOutput());
+            throw new RuntimeException($result->errorOutput());
         }
 
         info("<info>[✔]</info> Created DNS record: {$this->hostname}");
     }
 
-    protected function createViteDnsRecord(bool $force = false): void
+    protected function createViteDnsRecord(): void
     {
-        $command = $this->option('force') ?? $force
-            ? "cloudflared tunnel route dns --overwrite-dns {$this->hostname} vite-{$this->hostname}"
-            : "cloudflared tunnel route dns {$this->hostname} vite-{$this->hostname}";
-
         $result = spin(
-            callback: fn () => Process::run($command),
+            callback: fn () => Process::run("cloudflared tunnel route dns --overwrite-dns {$this->hostname} vite-{$this->hostname}"),
             message: "Creating DNS record: vite-{$this->hostname} …"
         );
 
-        if ($result->seeInErrorOutput('Failed to add route: code: 1003')) {
-            $confirmed = confirm(
-                label: "A DNS record for vite-{$this->hostname} already exists. Overwrite?",
-                yes: 'Yes, overwrite the record.',
-                no: 'No, abort and delete the tunnel.',
-            );
-
-            if (!$confirmed) {
-                $this->callSilently(CloudflaredUninstall::class);
-                $this->fail('The tunnel was deleted.');
-            }
-
-            $this->createViteDnsRecord(true);
-
-            return;
-        }
-
         if ($result->failed()) {
-            throw new \RuntimeException($result->errorOutput());
+            throw new RuntimeException($result->errorOutput());
         }
 
         info("<info>[✔]</info> Created DNS record: vite-{$this->hostname}");
@@ -170,13 +134,13 @@ YAML;
     {
         $result = spin(
             callback: fn () => Process::run("herd link {$this->hostname}"),
-            message: 'Creating Laravel Herd link …'
+            message: 'Creating Herd link …'
         );
 
         if ($result->failed()) {
-            throw new \RuntimeException($result->errorOutput());
+            throw new RuntimeException($result->errorOutput());
         }
 
-        info("<info>[✔]</info> Created Laravel Herd link.");
+        info('<info>[✔]</info> Created Herd link.');
     }
 }
